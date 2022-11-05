652b0e38b82d135877297735d27bbbe4f12f3b3d
Dimitri Desvillechabrol <d.desvillechabrol@gmail.com>
2022-11-05 14:59:18 +0100

    add tmux

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,19 +1,23 @@
+set -gx TERM "xterm-256color"
 set fish_prompt_pwd_dir_length 3
 
 # set anaconda
 source /home/desvillechabrol/miniconda3/etc/fish/conf.d/conda.fish 
 
 # set pyenv
+set -gx PATH "$HOME/.pyenv/bin" $PATH
 pyenv init - | source
 
 # set rust
-set -gx PATH "$HOME/.cargo/bin:$PATH"
+set -gx PATH "$HOME/.cargo/bin" $PATH
 
 # aliases
 alias nvdiff "nvim -d"
 
-set -gx TERM "xterm-256color"
+# set volta
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+# set local bin
 set -gx PATH "$HOME/.local/bin" $PATH
 
