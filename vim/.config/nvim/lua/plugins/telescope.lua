c8f7efd3dafeac9e0a10075cdb976c3d73ce725d
Dimitri Desvillechabrol <d.desvillechabrol@gmail.com>
2024-07-27 14:51:42 +0200

    ⬆️ ️upgrade telescope

--- vim/.config/nvim/lua/plugins/telescope.lua
+++ vim/.config/nvim/lua/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
   },
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.2',
+    branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local actions = require("telescope.actions")
