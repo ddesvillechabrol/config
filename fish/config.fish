510c61d67a91287a013826ff4253144bfebf5b7d
Dimitri Desvillechabrol <d.desvillechabrol@gmail.com>
2025-07-28 10:18:13 +0200

    🔧 activate ssh agent with fish

--- fish/config.fish
+++ fish/config.fish
@@ -15,7 +15,20 @@ set -gx PATH "$VOLTA_HOME/bin" $PATH
 # set local bin
 set -gx PATH "$HOME/.local/bin" $PATH
 
+# set rust
+set -gx PATH "$HOME/.cargo/bin" $PATH
+
+# set zig
+set -gx PATH "$HOME/Software/zig" $PATH
+
 # aliases
 alias nvdiff "nvim -d"
 
+# add key if absent from ssh-agent
+ssh-add -l > /dev/null 2>&1
+if begin; test $status -eq 1; and __ssh_agent_is_started; end
+    ssh-add $HOME/.ssh/id_ed25519
+end
+
+uv generate-shell-completion fish | source
 starship init fish | source
